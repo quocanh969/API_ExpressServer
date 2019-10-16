82afae7134398a8ea71df4a911a48aa7667fd50c
quocanh969 <tranquocanh858@gmail.com>
2019-10-16 16:56:59 +0700

    finish login post request

---
+++ passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const  passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+const User = require('./connecDB');
+
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'name',
+        passwordField: 'password',
+    },
+    function (name, password, cb){
+        console.log(name + " x " + password);
+        return User.findOne({where:{name,password}})
+            .then(user => {                
+                if(!user)
+                {
+                    return cb(null, false, {message: 'Incorrect name and password'});
+                }
+                else
+                {   
+                    return cb(null, {id:user.id,name:user.name, password: user.password}, {message:'Logged in successfully'});
+                }
+            })
+            .catch(err => {
+                console.log("error in passport: " + err);
+                return cb(err)
+            });
+    }
+));
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
 var User = require('../connecDB');
 var router = express.Router();
 
@@ -10,23 +12,62 @@ router.get('/',function(req,res,next) {
   res.json("hello user");
 })
 
-/* GET users listing. */
 router.post('/register', function(req, res, next) {
   const {name, password} = req.body;
-  createNewUser({name, password})
-  .then(
-    user => {
+
+  User.findOne({
+    where: {name},
+  })
+  .then(user=>{
+    if(user !== null)
+    {
       res.json({
-        user:user,
-        message:"Register success !!!",
+        message:"This name is already exist !!!",
       })
-    },
-    err => {
-      res.json({
-        message:"Register fail !!!",
-      })
+    }   
+    else
+    {
+      createNewUser({name, password})
+      .then(
+        user => {
+          res.json({
+            user:user,
+            message:"Register success !!!",
+          })
+        },
+        err => {
+          res.json({
+            message:"Register fail !!!",
+          })
+        }
+      )
+    } 
+  })
+  .catch()
+});
+
+
+router.post('/login', function(req, res, next) {  
+  passport.authenticate('local', {session: false}, (err, user, info)=>{        
+    if(err || !user)
+    {   
+      console.log(user);
+        return res.status(400).json({
+            message:'Something is not right',
+            user: user,
+        });
     }
-  )
+
+    req.login(user, {session: false}, (err)=>{
+        if(err)
+        {
+            res.send(err);
+        }
+        let payload = {id:user.id};
+        const token = jwt.sign(payload,'your_jwt_serect');
+        return res.json({user,token,info});
+    });
+  })(req,next);
 });
 
 module.exports = router;
