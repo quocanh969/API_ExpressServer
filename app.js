f21acdfe465e5dea9352ad0a9ce3cfecec19fd24
quocanh969 <tranquocanh858@gmail.com>
2019-10-16 16:23:34 +0700

    register successfully

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var User = require('./connecDB');
-
 var app = express();
 
 // view engine setup
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var User = require('../connecDB');
 var router = express.Router();
 
+const createNewUser = async ({ name, password }) => {
+  return await User.create({ name, password });
+};
+
+router.get('/',function(req,res,next) {
+  res.json("hello user");
+})
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function(req, res, next) {
+  const {name, password} = req.body;
+  createNewUser({name, password})
+  .then(
+    user => {
+      res.json({
+        user:user,
+        message:"Register success !!!",
+      })
+    },
+    err => {
+      res.json({
+        message:"Register fail !!!",
+      })
+    }
+  )
 });
 
 module.exports = router;
